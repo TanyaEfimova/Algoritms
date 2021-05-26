ad170ffd75cfb6ff3fb336bb6995192f5a36f05b
TanyaEfimova <mail5x5@mail.ru>
2021-05-26 05:46:04 +0300

    Задание 12.1.5

--- Algoritms/Program.cs
+++ Algoritms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Algoritms
 {
@@ -6,6 +8,35 @@ namespace Algoritms
     {
         static void Main(string[] args)
         {
+            List<User> users = new List<User>();
+
+            users.Add(new User { Login = "tanya", Name = "Танюшка", IsPremium = false });
+
+            Console.WriteLine("Введите свой логин");
+            var login = Console.ReadLine();
+
+            var curUser = users.Find((x) => x.Login == login);
+            Console.WriteLine("Привет, " + curUser.Name);
+
+            if (!curUser.IsPremium)
+            {
+                ShowAds();
+            }
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 }
